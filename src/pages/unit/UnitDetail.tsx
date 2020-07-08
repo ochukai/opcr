93ab6e20ada3577295101dc9fe94e8d1c16476e9
Oliver <sex.xiaoyee@gmail.com>
2020-07-08 23:59:21 +0800

    ~

--- src/pages/unit/UnitDetail.tsx
+++ src/pages/unit/UnitDetail.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Select, Divider } from 'antd';
+import _ from 'lodash';
 
-import ranks from '../../assets/json/rank.json';
-import units from '../../assets/json/unit.json';
-import { Card, Row, Col, Divider } from 'antd';
 import Equipment from '../../components/equip/Equipment';
 import Unit from '../../components/unit/Unit';
 
 import './unitDetail.less';
 
+import ranks from '../../assets/json/rank.json';
+import units from '../../assets/json/unit.json';
+
+const { Option } = Select;
+
 function UnitDetail() {
   const { id } = useParams();
+  const [rank, setRank] = useState({ value: 8, label: 'RANK8' });
 
+  const defaultId = 999999;
   const idNum = parseInt(id);
   const myRanks = ranks.filter(r => r.id === idNum);
   const me = units.filter(u => u.id === idNum)[0];
-  const defaultId = 999999;
+  const rankNumbers = myRanks.map(mr => ({ value: mr.rank, label: `RANK${mr.rank}` }));
+
+  const rankData = myRanks.filter(mr => mr.rank === rank.value)[0];
+
+  const handleRankChange = (value: any) => {
+    console.log('handle rank change', value);
+    setRank(value);
+  };
 
   return (
     <div className="opcr-unit-detail">
@@ -29,34 +42,39 @@ function UnitDetail() {
         </div>
       </div>
       <Divider />
-      <div className="outer-rank-wrapper">
+      <Select
+        listHeight={480}
+        labelInValue={true}
+        value={rank as any}
+        onChange={handleRankChange}
+      >
         {
-          myRanks.map((mr, index) => {``
-            return (
-              <div className="rank-wrapper">
-                <span className="rank">RANK{mr.rank}</span>
-                <div className="rank-item eq1">
-                  <Equipment size={48} id={mr.id1 || defaultId} />
-                </div>
-                <div className="rank-item eq2">
-                  <Equipment size={48} id={mr.id2 || defaultId} />
-                </div>
-                <div className="rank-item eq3">
-                  <Equipment size={48} id={mr.id3 || defaultId} />
-                </div>
-                <div className="rank-item eq4">
-                  <Equipment size={48} id={mr.id4 || defaultId} />
-                </div>
-                <div className="rank-item eq5">
-                  <Equipment size={48} id={mr.id5 || defaultId} />
-                </div>
-                <div className="rank-item eq6">
-                  <Equipment size={48} id={mr.id6 || defaultId} />
-                </div>
-              </div>
-            );
-          })
+          rankNumbers.map(op => <Option value={op.value} key={op.value}>{op.label}</Option>)
         }
+      </Select>
+
+      <div className="outer-rank-wrapper">
+        <div className="rank-wrapper">
+          <span className="rank">{rank.label}</span>
+          <div className="rank-item eq1">
+            <Equipment size={64} id={rankData.id1 || defaultId} />
+          </div>
+          <div className="rank-item eq2">
+            <Equipment size={64} id={rankData.id2 || defaultId} />
+          </div>
+          <div className="rank-item eq3">
+            <Equipment size={64} id={rankData.id3 || defaultId} />
+          </div>
+          <div className="rank-item eq4">
+            <Equipment size={64} id={rankData.id4 || defaultId} />
+          </div>
+          <div className="rank-item eq5">
+            <Equipment size={64} id={rankData.id5 || defaultId} />
+          </div>
+          <div className="rank-item eq6">
+            <Equipment size={64} id={rankData.id6 || defaultId} />
+          </div>
+        </div>
       </div>
     </div>
   );
