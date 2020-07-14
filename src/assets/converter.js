fb06e13707566738232fa61e0040d26c10bb24da
Oliver <sex.xiaoyee@gmail.com>
2020-07-14 23:45:22 +0800

    webp -> png

---
+++ src/assets/converter.js
@@ -0,0 +1,36 @@
+const webp = require('webp-converter');
+const path = require('path');
+const fse = require('fs-extra');
+
+async function convert(from, to, name) {
+  const toName = path.join(toDir, to, `${name}.png`);
+  const result = await webp.dwebp(from, toName, '-o');
+  console.log(result);
+}
+
+const fromDir = path.join(__dirname, 'images');
+const toDir = path.join(__dirname, 'png-images');
+
+async function readDir(dir) {
+  const pas = fse.readdirSync(dir);
+  for (let i = 0; i < pas.length; i ++) {
+    const ele = pas[i];
+    const innerDir = path.join(dir, ele);
+    const info = fse.statSync(innerDir);
+    if (info.isDirectory()) {
+      readDir(innerDir);
+    } else {
+      console.log('file', innerDir);
+
+      const [name, suffix] = ele.split('.');
+      if (suffix === 'webp') {
+        const cds = dir.split('\\');
+        await convert(innerDir, cds.pop(), name);
+      }
+    }
+  }
+}
+
+fse.ensureDirSync(toDir);
+readDir(fromDir);
+
